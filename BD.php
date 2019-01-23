efb35b96e18010a50a8864457125917bce69daa5
Ainhoa <Ainhoa@Ainhoa>
2019-01-23 22:03:22 +0100

    Creación archivo estilos y avance en index y BD

--- BD.php
+++ BD.php
@@ -6,3 +6,42 @@
  * and open the template in the editor.
  */
 
+class BD {
+    
+    /**
+     * Atributos de la clase BD
+     */
+    private $conexion;
+    private $host;
+    private $user;
+    private $pass;
+    private $bd;
+    private $info;
+    
+    /**
+     * Constructor de la clase BD al que le pasamos los siguientes parámetros
+     * @param type $host
+     * @param type $user
+     * @param type $pass
+     * @param type $bd
+     */
+    public function __construct($host, $user, $pass, $bd) {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->bd = $bd;
+        $this->conexion = $this->conexion();
+    }
+
+    /**
+     * 
+     * @return \mysqli
+     */
+    private function conexion(){
+        $conexion = new mysqli($this->host, $this->user, $this->pass, $this->bd);
+        if ($conexion->connect_errno){
+            $this->info = "Error conectando...<b>" . $conexion->connect_error . "</b>";
+        }
+        return $conexion;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * Se utiliza el spl_autoload_register para incluir lo diferentes 
+ * archivos-clases que se utilizan en el programa
+ */
+spl_autoload_register(function($nombre_clase) {
+    require $nombre_clase . '.php';
+});
+
+session_start();
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -7,11 +19,36 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Gestor Base de Datos</title>
+        <link rel="stylesheet" type="text/css" href="estilos.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>Gestor de base de datos</h1>
+        <fieldset>
+            <legend>Acceso para la BD</legend>
+            <div>
+                <?php
+                // put your code here
+                ?>
+            </div>
+            <br/>
+            <form name="acceso" action="." method="POST" enctype="multipart/form-data">
+                <div class="host">
+                    <label>Host</label>
+                    <input type="text" name="host">
+                </div>
+                <div class="usuario">
+                    <label>Usuario</label>
+                    <input type="text" name="usuario">
+                </div>
+                <div class="password">
+                    <label>Password</label>
+                    <input type="password" name="password">
+                </div>
+                <br/>
+                <input type="submit" value="Conectar" name="login" class="btnConectar"/>
+            </form>
+        </fieldset>
+
     </body>
 </html>
